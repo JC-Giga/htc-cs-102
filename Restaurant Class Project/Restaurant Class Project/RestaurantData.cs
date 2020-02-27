5431b3584976c410138ea83807084abf5b12db56
JC JC <JC12525400@gmail.com>
2020-02-26 19:27:53 -0500

    Restaurant Project Created

---
+++ Restaurant Class Project/Restaurant Class Project/RestaurantData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Class_Project
+{
+    class RestaurantData
+    {
+        public string resName;
+        public string resLeader;
+        public int numEmployees;
+        //display phone number when text area clicked
+        public void
+
+
+    }
+}
